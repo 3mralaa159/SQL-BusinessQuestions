bb25aecc5cec8e141f8cfedef11220c76f55ca5c
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-06-30 22:00:40 +0300

    Update No-1.sql

--- SQL/No-1.sql
+++ SQL/No-1.sql
@@ -1,7 +1,7 @@
 # Question:
- - You're tasked with analyzing a Spotify-like dataset that captures user listening habits.
- - For each user, calculate the total listening time and the count of unique songs they've listened to. In the database duration values are displayed in seconds. Round the total listening     
- - duration to the nearest whole minute.
+  You're tasked with analyzing a Spotify-like dataset that captures user listening habits.
+  For each user, calculate the total listening time and the count of unique songs they've listened to. In the database duration values are displayed in seconds. Round the total listening     
+  duration to the nearest whole minute.
 
 # Answer:
 select user_id , count(distinct song_id) as unique_song_count ,
