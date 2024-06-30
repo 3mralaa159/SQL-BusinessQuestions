9bc8f6193a57693e73afe2c022872c17bf5971e4
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-06-30 22:05:23 +0300

    Update No-1.sql

--- SQL/No-1.sql
+++ SQL/No-1.sql
@@ -3,8 +3,9 @@
   For each user, calculate the total listening time and the count of unique songs they've listened to. In the database duration values are displayed in seconds. Round the total listening     
   duration to the nearest whole minute.
 # link
-  
-  [link](https://platform.stratascratch.com/coding/10367-aggregate-listening-data?code_type=1/).
+  [Link to Aggregate Listening Data](https://platform.stratascratch.com/coding/10367-aggregate-listening-data?code_type=1)
+
+ 
 # Answer:
 select user_id , count(distinct song_id) as unique_song_count ,
 ceil(sum(listen_duration)/60) as total_listen_duration
