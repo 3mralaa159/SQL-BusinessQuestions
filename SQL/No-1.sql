d3e9f982974cbb99f4d6ab1f5e4cf248d99619d2
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-06-30 21:59:39 +0300

    Update and rename No.1 to No-1.sql

---
+++ SQL/No-1.sql
@@ -0,0 +1,11 @@
+# Question:
+  You're tasked with analyzing a Spotify-like dataset that captures user listening habits.
+  For each user, calculate the total listening time and the count of unique songs they've listened to. In the database duration values are displayed in seconds. Round the total listening     
+  duration to the nearest whole minute.
+
+Answer:
+select user_id , count(distinct song_id) as unique_song_count ,
+ceil(sum(listen_duration)/60) as total_listen_duration
+from listening_habits
+group by user_id 
+order by 3 desc
