cbf6de4df9df7084064570267e3a6ea55baaec10
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-07-04 14:42:23 +0300

    Create 2-Weekly Orders Report.sql

---
+++ SQL/2-Weekly Orders Report.sql
@@ -0,0 +1,12 @@
+# Question
+For each week, find the total number of orders. Include only the orders that are from the first quarter of 2023
+# link
+https://platform.stratascratch.com/coding/10363-weekly-orders-report?code_type=1
+# Answer
+select week , sum(quantity)
+from orders_analysis
+where date_part('quarter',week) = 1 
+group by 1 
+order by 1 
+
+
