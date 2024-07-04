292e95c2cafb7c33fd55b2631c2b2f70a290da86
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-07-04 15:15:51 +0300

    Create 2-Weekly Orders Report.py

---
+++ Python/2-Weekly Orders Report.py
@@ -0,0 +1,20 @@
+# Question
+For each week, find the total number of orders. Include only the orders that are from the first quarter of 2023.
+# link
+https://platform.stratascratch.com/coding/10363-weekly-orders-report?code_type=2
+
+# Answer
+# Import your libraries
+import pandas as pd
+# code
+orders_analysis.head()
+# sum by week 
+total = orders_analysis.groupby('week')['quantity'].sum()
+# create new dataframe with each week and sum of quantity
+df = pd.DataFrame({'week':total.index ,
+'quantity':total.values
+})
+# change into date format to filter 
+df['week'] = pd.to_datetime(df['week'])
+df['week'] = df['week'].dt.strftime('%Y-%m-%d')
+df[df['week'] < '2023-04-01']
