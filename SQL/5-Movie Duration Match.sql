aeefd6ec3cb2c71c2ef1298b66bbc85ecda89fe2
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-07-06 15:23:08 +0300

    Update 5-Movie Duration Match.sql

--- SQL/5-Movie Duration Match.sql
+++ SQL/5-Movie Duration Match.sql
@@ -2,7 +2,7 @@
     As a data scientist at Amazon Prime Video, you are tasked with enhancing the in-flight entertainment experience 
     for Amazon’s airline partners. Your challenge is to develop a feature that suggests individual movies from Amazon's content database that fit within a given flight's duration.
     For flight 101, find movies whose runtime is less than or equal to the flight's duration.
-    The output should list suggested movies for the flight, including 'flight_id', 'movie_id', and 'movie_duration'."
+    The output should list suggested movies for the flight, including 'flight_id', 'movie_id', and 'movie_duration'.'
 # Link
   https://platform.stratascratch.com/coding/10360-movie-duration-match?code_type=1
 # Answer
