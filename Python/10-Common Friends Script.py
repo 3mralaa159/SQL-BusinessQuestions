0fd887bfb3b1b8c78e967f3c168c2cbf1e868fab
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-08-11 14:14:54 +0300

    Update 10-Common Friends Script.py

--- Python/10-Common Friends Script.py
+++ Python/10-Common Friends Script.py
@@ -21,7 +21,7 @@ mutual_friend_ids = friends_of_karl[friends_of_karl.isin(friends_of_hans)]
 # Step 3: Get the details of mutual friends from the users DataFrame
 mutual_friends_df = users[users['user_id'].isin(mutual_friend_ids)]
 print(mutual_friends_df)
-
+-------------------------------------------------------------------
 # Answer.2
 # Step 1: Find the user IDs of Karl and Hans
 karl_id = users.loc[users['user_name'] == 'Karl', 'user_id'].iloc[0]
