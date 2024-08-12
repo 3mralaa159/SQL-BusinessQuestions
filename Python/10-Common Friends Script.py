6def3f035d7242178449fb4bd42e3fb655aac2d4
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-08-12 18:08:21 +0300

    Update 10-Common Friends Script.py

--- Python/10-Common Friends Script.py
+++ Python/10-Common Friends Script.py
@@ -23,6 +23,7 @@ mutual_friends_df = users[users['user_id'].isin(mutual_friend_ids)]
 print(mutual_friends_df)
 -------------------------------------------------------------------
 # Answer.2
+>> Query
 # Step 1: Find the user IDs of Karl and Hans
 karl_id = users.loc[users['user_name'] == 'Karl', 'user_id'].iloc[0]
 hans_id = users.loc[users['user_name'] == 'Hans', 'user_id'].iloc[0]
@@ -35,3 +36,50 @@ mutual_friend_ids = friends.query('friend_id == @karl_id or friend_id == @hans_i
 # Step 3: Get the details of mutual friends from the users DataFrame
 mutual_friends_df = users[users['user_id'].isin(mutual_friend_ids)]
 print(mutual_friends_df)
+-------------------------------------------------------------------
+# Answer.3
+>> Merge
+# Get user IDs
+karl_id = users[users['user_name'] == 'Karl']['user_id'].iloc[0]
+hans_id = users[users['user_name'] == 'Hans']['user_id'].iloc[0]
+
+# Karl's friends
+karl_friends = friends[friends['user_id'] == karl_id]
+# Hans' friends
+hans_friends = friends[friends['user_id'] == hans_id]
+
+# Merge to find mutual friends
+mutual_friends = pd.merge(karl_friends, hans_friends, on='friend_id')['friend_id']
+users[users['user_id'].isin(mutual_friends)]
+-------------------------------------------------------------------
+# Answer.4
+>> intersection + groupby using set method
+karl_id = users[users['user_name'] == 'Karl']['user_id'].iloc[0]
+hans_id = users[users['user_name'] == 'Hans']['user_id'].iloc[0]
+
+friends_grouped = friends.groupby('user_id')['friend_id'].apply(set)
+mutual_friends = friends_grouped[karl_id].intersection(friends_grouped[hans_id])
+
+users[users['user_id'].isin(mutual_friends)]
+-------------------------------------------------------------------
+# Answer.5
+>> intersection + set method 
+karl_id = users[users['user_name'] == 'Karl']['user_id'].iloc[0]
+hans_id = users[users['user_name'] == 'Hans']['user_id'].iloc[0]
+
+karl_friends_set = set(friends[friends['user_id'] == karl_id]['friend_id'])
+hans_friends_set = set(friends[friends['user_id'] == hans_id]['friend_id'])
+
+mutual_friends = karl_friends_set.intersection(hans_friends_set)
+users[users['user_id'].isin(mutual_friends)]
+-------------------------------------------------------------------
+# Answer.6
+>> Self join
+karl_id = users[users['user_name'] == 'Karl']['user_id'].iloc[0]
+hans_id = users[users['user_name'] == 'Hans']['user_id'].iloc[0]
+
+friends_self_join = pd.merge(friends, friends, on='friend_id')
+mutual_friends = friends_self_join[(friends_self_join['user_id_x'] == karl_id) & 
+                                   (friends_self_join['user_id_y'] == hans_id)]['friend_id']
+
+users[users['user_id'].isin(mutual_friends)]
