a6eda0c551afd7b7e2f7ce71b3af34242f62f209
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-08-22 14:46:26 +0300

    Create 16-Distances Traveled.py

---
+++ Python/16-Distances Traveled.py
@@ -0,0 +1,7 @@
+# Question
+  Find the top 10 users that have traveled the greatest distance. Output their id, name and a total distance traveled.
+
+# Link
+  https://platform.stratascratch.com/coding/10324-distances-traveled?code_type=1
+
+# Answer
