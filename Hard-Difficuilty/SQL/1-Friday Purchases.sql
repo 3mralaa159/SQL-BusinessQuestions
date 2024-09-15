c545011586a23be24a8f975d939436d59c76b795
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-09-15 09:08:17 +0300

    Update 1-Friday Purchases.sql

--- Hard-Difficuilty/SQL/1-Friday Purchases.sql
+++ Hard-Difficuilty/SQL/1-Friday Purchases.sql
@@ -1,1 +1,20 @@
+# Question
+IBM is working on a new feature to analyze user purchasing behavior for all Fridays in the first quarter of the year. For each Friday separately,
+  calculate the average amount users have spent per order. The output should contain the week number of that Friday and average amount spent.
 
+# Link
+https://platform.stratascratch.com/coding/10358-friday-purchases?code_type=1
+
+# Answer
+SELECT 
+    EXTRACT(WEEK FROM date) AS week_number, 
+    AVG(amount_spent) AS avg_amount_spent
+FROM 
+    your_table_name
+WHERE 
+    EXTRACT(QUARTER FROM date) = 1
+    AND day_name = 'Friday'
+GROUP BY 
+    week_number
+ORDER BY 
+    week_number;
