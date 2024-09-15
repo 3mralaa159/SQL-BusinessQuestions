b93e5f9da53cae8cd9d1c5cb0c93b587b29d96c1
3mralaa159 <91030873+3mralaa159@users.noreply.github.com>
2024-09-15 09:17:00 +0300

    Update 1-Friday Purchases.py

--- Hard-Difficuilty/Python/1-Friday Purchases.py
+++ Hard-Difficuilty/Python/1-Friday Purchases.py
@@ -1,1 +1,13 @@
+# Question
+IBM is working on a new feature to analyze user purchasing behavior for all Fridays in the first quarter of the year. For each Friday separately,
+  calculate the average amount users have spent per order. The output should contain the week number of that Friday and average amount spent.
 
+# Link
+https://platform.stratascratch.com/coding/10358-friday-purchases?code_type=1
+
+# Answer
+df['quarter'] = df['date'].dt.quarter
+df['week_number'] = df['date'].dt.isocalendar().week
+df['is_friday'] = df['date'].dt.dayofweek == 4  # Friday is day 4
+df_friday_q1 = df[(df['quarter'] == 1) & (df['is_friday'])]
+df_friday_q1.groupby('week_number')['amount_spent'].mean().reset_index()
